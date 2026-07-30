88ee932696ca775116899c80c433653d8100ee4e
Souvik <dasouvik122005@gmail.com>
2026-07-30 12:58:23 +0530

    updated

--- todo/src/environments/environment.prod.ts
+++ todo/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'YOUR_VERCEL_BACKEND_URL/api' // We will replace this later
+  apiUrl: 'https://todolist-zeta-pink.vercel.app/' // We will replace this later
 };
