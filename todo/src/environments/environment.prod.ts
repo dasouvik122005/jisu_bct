48b7bead2021b938ed2faefae3cb49f3d13a88b9
Souvik <dasouvik122005@gmail.com>
2026-07-30 13:19:02 +0530

    Update production environment with live backend URL

--- todo/src/environments/environment.prod.ts
+++ todo/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://todolist-zeta-pink.vercel.app/' // We will replace this later
+  apiUrl: 'https://todolist-zeta-pink.vercel.app/api'
 };
